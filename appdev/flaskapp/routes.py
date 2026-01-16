a06868ae5eba75d33b520d74d51f6eb779ae50ca
Gokul-2k7 <k.gokul27112007@gmail.com>
2026-01-16 20:59:40 +0530

    fix:redirect to home after login

--- appdev/flaskapp/routes.py
+++ appdev/flaskapp/routes.py
@@ -4,14 +4,14 @@ from flaskapp import app
 from flaskapp.models import db,User,Post
 
 @app.route('/',methods=["GET","POST"])
-def home():
+def login():
     error=None
     if request.method=="POST":
         name=request.form.get("name")
         password=request.form.get("password")
         user = User.query.filter_by(username=name).first()
         if user and user.password == password:
-            return f"HI {name}"
+            return redirect(url_for('home',name=name))
         else:
            error="Invalid username or password"        
     return render_template('index.html',error=error)
@@ -28,9 +28,9 @@ def register():
         user=User(username=name,email=f"{name}@gmail.com",password=password)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html')
 
-@app.route('/name=<name>/age=<age>')
-def details(name,age):
-    return f"{name} age is {age}"
+@app.route('/home')
+def home(name):
+    return f"Hi {name}\nThis is the home page"
