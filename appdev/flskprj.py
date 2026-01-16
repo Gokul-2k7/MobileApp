5a20bf10d5f2d83fbfbe154ebee420c297b9c700
Gokul-2k7 <k.gokul27112007@gmail.com>
2026-01-16 12:42:46 +0530

    fix:some_changes

--- appdev/flskprj.py
+++ appdev/flskprj.py
@@ -2,12 +2,13 @@ import os
 from flask import Flask,request,render_template,redirect,url_for
 app=Flask(__name__)
 login={}
+posts={}
 @app.route('/',methods=["GET","POST"])
 def home():
     error=None
     if request.method=="POST":
         name=request.form.get("name")
-        Password=request.form.get("Pass")
+        Password=request.form.get("password")
         if name in login and login[name]==Password:
             return f"HI {name}"
         else:
@@ -17,14 +18,14 @@ def home():
 def register():
     Name=None
     if request.method=="POST":
-        Name=request.form.get("Name")
-        Pass=request.form.get("Pass")
+        Name=request.form.get("name")
+        Pass=request.form.get("password")
         if Name in login:
             error="Username exists"
-            return render_template('register.html',login=login,Name=Name,error=error)
+            return render_template('register.html',error=error)
         login[Name]=Pass
         return redirect(url_for('home'))
-    return render_template('register.html',login=login,Name=Name)
+    return render_template('register.html')
 
 @app.route('/name=<name>/age=<age>')
 def details(name,age):
@@ -32,4 +33,4 @@ def details(name,age):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
